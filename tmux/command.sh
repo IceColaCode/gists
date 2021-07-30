ddfa8dd4ec21864466b7be4f9f99dceada5d4791
luoyan <luo.yan@hylinkad.com>
2021-07-30 10:29:17 +0800

    add tmux

---
+++ tmux/command.sh
@@ -0,0 +1,17 @@
+# create
+tmux new -s session-name
+# detach
+tmux detach
+Ctrl+b d
+# ls
+tmux ls
+# attach
+tmux attach -t session-name
+# kill
+tmux kill-session -t session-name
+# 切换会话
+tmux switch -t session-name
+#划分左右两个窗格。
+Ctrl+b %
+#划分上下两个窗格。
+Ctrl+b "
